19c523649e48b2589de09cfc420f9dc8c819b984
uditangshu <145389897+uditangshu@users.noreply.github.com>
2024-08-05 11:32:07 +0530

    update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-import WebSocket, { WebSocketServer } from 'ws';
-import http from 'http';
+import express from 'express'
+import { WebSocketServer } from 'ws'
 
-const server = http.createServer(function(request: any, response: any) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.end("hi there");
-});
+const app = express()
+const httpServer = app.listen(8080)
 
-const wss = new WebSocketServer({ server });
+const wss = new WebSocketServer({ server: httpServer });
 
 wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
@@ -18,9 +16,6 @@ wss.on('connection', function connection(ws) {
       }
     });
   });
-  ws.send('Hello! Message From Server!!');
-});
 
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
+  ws.send('Hello! Message From Server!!');
 });
